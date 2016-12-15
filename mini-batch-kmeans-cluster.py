00f230a8cb6b65b7e5a83577d79711eb379c5fc8
Eric Devine <devineej@gmail.com>
2016-12-14 23:08:18 -0500

    Add morough map to clustering

--- mini-batch-kmeans-cluster.py
+++ mini-batch-kmeans-cluster.py
@@ -1,9 +1,27 @@
 import os
+import json
 from sklearn.cluster import MiniBatchKMeans
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 from scipy.spatial import ConvexHull
 
+with open('geo-data/borough-boundaries.json') as file:
+    boroBoundaries = json.load(file)
+
+boros = boroBoundaries['features']
+colors = plt.cm.Spectral(np.linspace(0, 1, len(boros)))
+
+# Draw map
+for boro in boros:
+    for shape in boro['geometry']['coordinates']:
+        for line in shape:
+            x = [pt[0] for pt in line]
+            y = [pt[1] for pt in line]
+            plt.fill(x, y, color='black', alpha=0.3)
+            plt.plot(x, y, '-', color='black', lw=1)
+
+# Read taxi data
 points = list()
 for filename in os.listdir('taxi-data'):
     with open(os.path.join('taxi-data', filename)) as file:
@@ -17,6 +35,7 @@ for filename in os.listdir('taxi-data'):
 
 points = np.array(points)
 
+# Cluster taxi data
 n_clusters = 30
 kmeans = MiniBatchKMeans(n_clusters=n_clusters).fit(points)
 
@@ -25,6 +44,7 @@ unique_labels = set(labels)
 
 cluster_centers = kmeans.cluster_centers_
 
+# Plot taxi points
 colors = plt.cm.Spectral(np.linspace(0, 1, n_clusters))
 
 for label, color in zip(unique_labels, colors):
@@ -36,12 +56,12 @@ for label, color in zip(unique_labels, colors):
 
 
     plt.fill(cluster[vertices,1], cluster[vertices,0], color=color, alpha = 0.5)
-    plt.plot(cluster[vertices,1], cluster[vertices,0], '-', color=color, lw=1, fillstyle='full')
+    plt.plot(cluster[vertices,1], cluster[vertices,0], '-', color=color, lw=1)
 
     plt.plot(center[1], center[0], 'o', markerfacecolor=color, markeredgecolor='black', markersize=6)
     #plt.plot(cluster[:, 1], cluster[:, 0], 'o', markerfacecolor=color, markeredgecolor='black', markersize=6)
 
 # TODO: Remove outliers
-plt.axis((-74.1,-73.7,40.6,40.9))
+plt.axis((-74.3,-73.7,40.5,40.9))
 plt.title('Mini Batch K-Means estimated clusters: %d' % n_clusters)
 plt.show()
