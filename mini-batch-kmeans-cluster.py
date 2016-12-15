527ed5cb319b43dd4e101f258b836b56de357360
Eric Devine <devineej@gmail.com>
2016-12-14 20:50:14 -0500

    Cluster all of 2015 using MiniBatchKMeans

--- mini-batch-kmeans-cluster.py
+++ mini-batch-kmeans-cluster.py
@@ -1,18 +1,19 @@
+import os
 from sklearn.cluster import MiniBatchKMeans
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import ConvexHull
 
 points = list()
-
-with open('taxi-data/yellow_tripdata_2015-01.csv') as file:
-    next(file)
-    for ln in file:
-        row = ln.strip().split(',')
-        long = float(row[5])
-        lat = float(row[6])
-        if long != 0 and lat != 0:
-            points.append((lat, long))
+for filename in os.listdir('taxi-data'):
+    with open(os.path.join('taxi-data', filename)) as file:
+        next(file)
+        for ln in file:
+            row = ln.strip().split(',')
+            long = float(row[5])
+            lat = float(row[6])
+            if long != 0 and lat != 0:
+                points.append((lat, long))
 
 points = np.array(points)
 
@@ -22,18 +23,23 @@ kmeans = MiniBatchKMeans(n_clusters=n_clusters).fit(points)
 labels = kmeans.labels_
 unique_labels = set(labels)
 
+cluster_centers = kmeans.cluster_centers_
+
 colors = plt.cm.Spectral(np.linspace(0, 1, n_clusters))
 
 for label, color in zip(unique_labels, colors):
     cluster = points[labels == label]
+    center = cluster_centers[label]
 
     hull = ConvexHull(cluster)
     vertices = np.append(hull.vertices, hull.vertices[0])
 
+
     plt.fill(cluster[vertices,1], cluster[vertices,0], color=color, alpha = 0.5)
     plt.plot(cluster[vertices,1], cluster[vertices,0], '-', color=color, lw=1, fillstyle='full')
 
-    plt.plot(cluster[:, 1], cluster[:, 0], 'o', markerfacecolor=color, markeredgecolor='k', markersize=6)
+    plt.plot(center[1], center[0], 'o', markerfacecolor=color, markeredgecolor='black', markersize=6)
+    #plt.plot(cluster[:, 1], cluster[:, 0], 'o', markerfacecolor=color, markeredgecolor='black', markersize=6)
 
 # TODO: Remove outliers
 plt.axis((-74.1,-73.7,40.6,40.9))
